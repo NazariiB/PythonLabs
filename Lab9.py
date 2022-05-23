c26c895cb5c57d313be001bc739e381114b60e2a
NazariiB <92984941+NazariiB@users.noreply.github.com>
2022-05-23 16:08:59 +0300

    Update Lab9.py

--- Lab9.py
+++ Lab9.py
@@ -7,7 +7,7 @@ class Product(Accounting):
     def __init__(self, name):
         super().__init__()
         self.name = name
-C:\Users\nazik\PycharmProjects\pythonProject1\Lab9.py
+        
 
 class Customer(Product):
     def __init__(self, name):
